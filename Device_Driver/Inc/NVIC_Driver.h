df388ac8f7cf3f6bbc2ed1ee4fc84828e0c06902
HariHaranio <hariharan.io7331@gmail.com>
2026-09-15 16:05:47 +0530

    Add: NVIC_Driver added.

---
+++ Device_Driver/Inc/NVIC_Driver.h
@@ -0,0 +1,17 @@
+#ifndef NVIC_DRIVER_H_
+#define NVIC_DRIVER_H_
+
+#include "stm32f412xG.h"
+
+#include "stdint.h"
+#include "stdbool.h"
+
+void NVIC_EnableIRQ(uint8_t IRQn);
+void NVIC_DisableIRQ(uint8_t IRQn);
+void NVIC_SetPendingIRQ(uint8_t IRQn);
+void NVIC_ClearPendingIRQ(uint8_t IRQn);
+bool NVIC_ReadActiveIRQ(uint8_t IRQn);
+void NVIC_SetIRQPriority(uint8_t IRQn, uint8_t priority);
+
+
+#endif /* NVIC_DRIVER_H_ */
